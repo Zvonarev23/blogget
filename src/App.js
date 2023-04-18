27f3a7d96b06b4827f72fc0c4b2ad45329df29df
Zvonarev23 <zvonarev.evgeniy@gmail.com>
2023-04-19 00:07:33 +0300

    created post-context, hook use-posts and updated style from post component

--- src/App.js
+++ src/App.js
@@ -1,14 +1,17 @@
 import Header from "./components/header/index.jsx";
 import Main from "./components/main/index.jsx";
 import { AuthContextProvider } from "./context/auth-context.jsx";
+import { PostContextProvider } from "./context/post-context.jsx";
 import { TokenContextProvider } from "./context/token-context.jsx";
 
 function App() {
   return (
     <TokenContextProvider>
       <AuthContextProvider>
-        <Header />
-        <Main />
+        <PostContextProvider>
+          <Header />
+          <Main />
+        </PostContextProvider>
       </AuthContextProvider>
     </TokenContextProvider>
   );
